5f9ccb162ca306b28f927ddba21e7646e27e7618
Alraffi28 <mohammedalraffi@gmail.com>
2025-12-26 12:17:17 +0530

    edited files

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://whatsapp-clone-uh55.onrender.com"
+  baseURL: "https://whatsapp-clone-uh55.onrender.com/wp"
 });
 
 API.interceptors.request.use((req) => {
--- Front-end/src/socket/socket.js
+++ Front-end/src/socket/socket.js
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client";
-const socket = io("https://whatsapp-clone-uh55.onrender.com" , {
+const socket = io("https://whatsapp-clone-uh55.onrender.com/wp" , {
     transports : ["websocket"]
 })
 export default socket
